6fc6531811a356f84708808f98dc461cb3ce7d5e
Yuki <yuki@dawnlabsai.com>
2026-05-09 15:33:44 +0200

    remove how we work / value props section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -490,7 +490,6 @@ export default function Home() {
           <Metrics />
           <Services />
           <CaseStudy />
-          <ValueProps />
           <CTASection />
         </main>
         <Footer />
